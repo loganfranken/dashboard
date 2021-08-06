8f672f976edf8806b6a0b300672a8b5c074b4113
Logan Franken <loganfranken@gmail.com>
2021-08-06 09:25:00 -0700

    Style refactoring for the key press widget

--- src/getUserBehaviorSummary.js
+++ src/getUserBehaviorSummary.js
@@ -3,7 +3,7 @@ export default (callback) => {
     const goals = [
         { measure: 'seconds', target: 10 },
         { measure: 'clicks', target: 10 },
-        { measure: 'mouseHoldLength', target: 3 },
+        { measure: 'mouseHoldLength', target: 1 },
         { measure: 'keyPresses', target: 9 },
         { measure: 'mouseDistance', target: 10000 },
         { measure: 'uniqueKeyPresses', target: 20 },
