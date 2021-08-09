e7ad2351504a72d2a79a88de4ab6225bdf33665f
Logan Franken <loganfranken@gmail.com>
2021-08-09 10:33:19 -0700

    Small goal adjustments

--- src/getUserBehaviorSummary.js
+++ src/getUserBehaviorSummary.js
@@ -2,11 +2,11 @@ export default (callback) => {
 
     const goals = [
         { measure: 'seconds', target: 10 },
-        { measure: 'mouseDistance', target: 10000 },
         { measure: 'clicks', target: 10 },
         { measure: 'mouseHoldLength', target: 1 },
         { measure: 'keyPresses', target: 9 },
-        { measure: 'uniqueKeyPresses', target: 20 },
+        { measure: 'mouseDistance', target: 5000 },
+        { measure: 'uniqueKeyPresses', target: 30 },
         { measure: 'mouseVelocity', target: 10 },
         { measure: 'windowResizePercentage', target: 30 },
         { measure: 'clickButtonRatio', target: 0.3 },
